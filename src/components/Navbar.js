5a98536ab813b6dddc90d801f10ea4c50ece8f33
SakshiDeosale <sakshideosale@gmail.com>
2026-07-29 14:08:32 +0530

    Achievements sec removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,19 +92,19 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/*<Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href=""
                 target="_blank"
                 rel="noreferrer"
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Education
+                <ImBlog style={{ marginBottom: "2px" }} /> Achievements
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item>*/}
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/SakshiDeosale/SakshiDeosale-Portfolio"
+                href="https://github.com/SakshiDeosale/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
