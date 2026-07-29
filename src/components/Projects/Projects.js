8e232371d56bc49c6da06256deda4a4bf1e3e947
SakshiDeosale <sakshideosale@gmail.com>
2026-07-29 14:46:13 +0530

    Styling Changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,7 +25,7 @@ function Projects() {
               imgPath={wanderlust}
               isBlog={false}
               title="WanderLust – Airbnb Clone"
-              description="Wanderlust is a full-stack travel accommodation web application inspired by Airbnb that allows users to discover, explore, and manage travel property listings. The platform enables users to browse destinations, view detailed property information, create and manage listings, and share reviews and ratings through a secure and user-friendly interface. It is designed to simplify property listing management while providing an engaging experience for travelers and hosts."          ghLink="https://github.com/SakshiDeosale/Airbnb-Clone"
+              description="Wanderlust is a full-stack travel accommodation web application inspired by Airbnb that allows users to discover, explore, and manage travel property listings. The platform enables users to browse destinations, view detailed property information, create and manage listings, and share reviews and ratings through a secure and user-friendly interface. It is designed to simplify property listing management while providing an engaging experience for travelers and hosts." 
               ghLink="https://github.com/SakshiDeosale/Airbnb-Clone"
               demoLink="https://airbnb-clone-feo0.onrender.com/"
             />
@@ -36,7 +36,7 @@ function Projects() {
               imgPath={QRimage}
               isBlog={false}
               title="QR-Based Emergency Information Sharing System"
-              description="Developed a QR-Based Emergency Information Sharing System, a web application that enables users to securely store personal, medical, vehicle, insurance, and emergency contact information linked to a unique QR code. During emergencies, authorized responders can scan the QR code to instantly access critical details, helping reduce response time and improve decision-making. The system also includes secure user authentication and role-based access control for safe and efficient information management."              ghLink="https://github.com/SakshiDeosale/QR-CodeBased-Emergency-Project"
+              description="Developed a QR-Based Emergency Information Sharing System, a web application that enables users to securely store personal, medical, vehicle, insurance, and emergency contact information linked to a unique QR code. During emergencies, authorized responders can scan the QR code to instantly access critical details, helping reduce response time and improve decision-making. The system also includes secure user authentication and role-based access control for safe and efficient information management." 
               ghLink="https://github.com/SakshiDeosale/QR-CodeBased-Emergency-Project"
               // demoLink="https://blogs.soumya-jit.tech/"
             />
@@ -47,7 +47,7 @@ function Projects() {
               imgPath={college}
               isBlog={false}
               title="Academic Project Explorer"
-              description="Developed Academic Project Explorer, a full-stack web application for the CSE department that serves as a centralized repository of academic projects. The platform enables students to search and explore previous projects to avoid duplication, while faculty members can securely add, update, and manage project records through role-based access, providing an efficient and organized project management system."             ghLink="https://github.com/SakshiDeosale/Academic-Project-Explorer-"           
+              description="Developed Academic Project Explorer, a full-stack web application for the CSE department that serves as a centralized repository of academic projects. The platform enables students to search and explore previous projects to avoid duplication, while faculty members can securely add, update, and manage project records through role-based access, providing an efficient and organized project management system."            
               ghLink="https://github.com/SakshiDeosale/Academic-Project-Explorer-"
             />
           </Col>
